b8efd9308945f7abcc93e7c108578c184429ec20
Facundo Mangioni <106379788+FacuMangi@users.noreply.github.com>
2023-07-25 12:05:48 -0300

    Add project files.

---
+++ HomeBankingMindHub/Models/DbInitializer.cs
@@ -0,0 +1,26 @@
+﻿using System.Linq;
+
+namespace HomeBankingMindHub.Models
+{
+    public class DbInitializer
+    {
+        public static void Initialize(HomeBankingContext context)
+        {
+            if (!context.Clients.Any())
+            {
+                var clients = new Client[]
+                {
+                    new Client {Email = "datainicial@gmail.com", FirstName = "Cosme", LastName = "Fulanito", Password = "Password"}
+                };
+
+                foreach (Client client in clients)
+                {
+                    context.Clients.Add(client);
+                }
+                //guardando
+                context.SaveChanges();
+                
+            }
+        }
+    }
+}
